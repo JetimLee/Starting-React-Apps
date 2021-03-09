d3e4f550a626ed741d5a384372d5910f9ee10c11
Gavin Coulson <72872223+JetimLee@users.noreply.github.com>
2021-03-09 16:44:56 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,34 +2,53 @@ import React from "react"; //not necessary to to this when setting up a componen
 import reactDom from "react-dom";
 import "./index.styles.css";
 
+const firstBook = {
+  img:
+    "https://images-na.ssl-images-amazon.com/images/I/81drfTT9ZfL._AC_UL200_SR200,200_.jpg",
+  title: "The Cat in the Hat",
+  author: "Dr. Seuss",
+};
+
+const secondBook = {
+  img:
+    "https://images-na.ssl-images-amazon.com/images/I/81WZ6QvGZ2L._AC_UL200_SR200,200_.jpg",
+  title: "It's Not Easy Being a Bunny",
+  author: "Marilyn Sadler",
+};
+
 const BookList = () => {
   return (
     <section className="bookList">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      >
+        <p>
+          lorem ipsum lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum{" "}
+          lorem ipsum{" "}
+        </p>
+      </Book>
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
     </section>
   );
 };
 
-const Book = () => {
+const Book = (props) => {
+  //these two are the exact same thing ^ and Below
+  const { img, title, author } = props;
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img src={img}></img>
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+      {props.children}
     </article>
   );
 };
 
-const Image = () => (
-  <img src="https://images-na.ssl-images-amazon.com/images/I/81drfTT9ZfL._AC_UL200_SR200,200_.jpg"></img>
-);
-
-const Title = () => {
-  return <h1>The Cat in the Hat</h1>;
-};
-
-const Author = () => <h2>Dr. Seuss</h2>;
 reactDom.render(<BookList />, document.getElementById("root"));
